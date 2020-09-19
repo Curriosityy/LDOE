ed9ed7dc14b9306e5e8481ddafecd74bfae78b96
Patryk Basa <patryk9595@gmail.com>
2020-09-19 14:33:34 +0200

    -init

---
+++ Assets/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Minion
+    [SerializeField] CharacterController _controller;
+    float _speed = 3;
+    void Update()
+    {
+        var moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime* _speed;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out var hit,Mathf.Infinity,1<<8))
+        {
+            var point = hit.point;
+            point.y = _controller.transform.position.y;
+            var dirVector = (point - _controller.transform.position);
+            _controller.transform.rotation = Quaternion.LookRotation(dirVector.normalized, Vector3.up);
+            Debug.DrawRay(_controller.transform.position, dirVector,Color.red);
+        }
+        _controller.Move(moveVector);
+
+    }
+}
