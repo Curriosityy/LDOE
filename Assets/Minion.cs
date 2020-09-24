2dc80b1467d75214ba25d45923a4b69214d88631
Patryk Basa <patryk9595@gmail.com>
2020-09-24 20:59:12 +0200

    -walk animation

---
+++ Assets/Minion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    [SerializeField] CharacterController characterController;
+    [SerializeField] Animator animator;
+    float speed=5;
+    void Start()
+    {
+        
+    }
+
+    public void Move(float x,float z)
+    {
+        characterController.SimpleMove(((transform.forward*z)+(transform.right*x)).normalized*speed);
+        animator.SetFloat("X", x);
+        animator.SetFloat("Z", z);
+    }
+
+    public void LookAt(Vector3 position)
+    {
+        var dir = position - transform.position;
+        characterController.transform.rotation = Quaternion.LookRotation(dir.normalized, Vector3.up);
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,20 +6,25 @@ public class Player : MonoBehaviour
 {
     //Minion
     [SerializeField] CharacterController _controller;
-    float _speed = 3;
+    [SerializeField] Minion _minion;
+    Weapon _holdedWeapon;
     void Update()
     {
-        var moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Time.deltaTime* _speed;
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(ray, out var hit,Mathf.Infinity,1<<8))
+        if (Physics.Raycast(ray, out var hit, Mathf.Infinity, 1 << 8))
         {
             var point = hit.point;
-            point.y = _controller.transform.position.y;
-            var dirVector = (point - _controller.transform.position);
-            _controller.transform.rotation = Quaternion.LookRotation(dirVector.normalized, Vector3.up);
-            Debug.DrawRay(_controller.transform.position, dirVector,Color.red);
+            point.y = _minion.transform.position.y;
+            // var dirVector = (point - _controller.transform.position);
+            _minion.LookAt(point);
         }
-        _controller.Move(moveVector);
-
+        _minion.Move(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
     }
+
+}
+
+public abstract class Weapon
+{
+    public abstract void Attack();
+    public abstract void Reload();
 }
