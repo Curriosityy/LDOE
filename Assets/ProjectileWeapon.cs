44926e45cf59d46a5d968d81950f86cbf1baad1f
Patryk Basa <patryk9595@gmail.com>
2020-09-25 19:53:47 +0200

    -Weapon

--- Assets/ProjectileWeapon.cs
+++ Assets/ProjectileWeapon.cs
@@ -1,16 +1,51 @@
 ﻿
 
+using System;
+using System.Collections;
 using UnityEngine;
-
 public class ProjectileWeapon : Weapon
 {
+    [SerializeField] float _weaponRange=250;
+    [SerializeField] int _damage=5;
+    [SerializeField] float _fireRate=0.3f;
+    [SerializeField] int _maxRounds = 30;
+    bool _canFire=true;
+    int _rounds = 0;
+    
+    public void Initialize()
+    {
+
+    }
+
     public override void Attack()
     {
-        Debug.Log("FIREEEE");
+        if (!_canFire)
+            return;
+        Debug.Log("FIRE");
+        StartCoroutine(FireRate());
+        if(_rounds<=0)
+        {
+            Debug.Log("EMPTY");
+            return;
+        }
+        _rounds--;
+        if(Physics.Raycast(new Ray(transform.position, transform.forward), out var hit, _weaponRange))
+        {
+            var damageable = hit.collider.GetComponent<IDamageable>();
+            damageable?.Hit(_damage);
+        }
+    }
+
+    private IEnumerator FireRate()
+    {
+        _canFire = false;
+        yield return new WaitForSeconds(_fireRate);
+        _canFire = true;
     }
 
     public override void Reload()
     {
         Debug.Log("Reload");
+        _rounds = _maxRounds;
     }
 }
