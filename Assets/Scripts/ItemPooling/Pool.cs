d334bb5b947cb4baf74839e8151b787623fcd27e
Patryk Basa <patryk9595@gmail.com>
2020-10-01 21:42:12 +0200

    -pooling system

---
+++ Assets/Scripts/ItemPooling/Pool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool<T> : MonoBehaviour where T:MonoBehaviour
+{
+    [SerializeField] T _prefab;
+    Queue<T> _pool;
+    Transform _transform;
+    private void Awake()
+    {
+        _pool = new Queue<T>();
+        _transform = transform;
+    }
+    public void AddObjecToPool(T objectToAdd)
+    {
+        _pool.Enqueue(objectToAdd);
+        objectToAdd.transform.SetParent(_transform);
+        gameObject.SetActive(false);
+    }
+
+    public T GetNotActiveObjectFromPool()
+    {
+        CreateNewObjectIfEmpty();
+        return _pool.Dequeue();
+    }
+
+    private void CreateNewObjectIfEmpty()
+    {
+        if (_pool.Count == 0)
+            CreateNewObiectInPool();
+    }
+
+    public T GetActiveOvjectFromPool()
+    {
+        CreateNewObjectIfEmpty();
+        var obj = _pool.Dequeue();
+        obj.gameObject.SetActive(true);
+        return obj;
+    }
+
+    private void CreateNewObiectInPool()
+    {
+        _pool.Enqueue(Instantiate(_prefab));
+    }
+}
